a0aeef9a4b3a328bcb96e98ae1cf08dbc9507299
Kamil Wilczek <kamil.van.wilczek@gmail.com>
2015-05-19 17:45:26 +0200

    256 terminal colors in bash variables.

--- bash/xterm256_colors.sh
+++ bash/xterm256_colors.sh
@@ -10,27 +10,29 @@
 
 # Output file (current directory) and text.
 OFILE='xterm256_colors_test.sh'
+OFILE_COLORS='terminal_256_colors'
 OTEXT='Sed ut perspiciatis unde omnis iste natus error sit voluptatem...'
 
 # Clearing the contents from previous runs.
-if [ -e $OFILE ]
+if [ -e $OFILE ] || [ -e $OFILE_COLORS ]
 then
-	echo "File $OFILE exists. Overwriting..."
 	> $OFILE
+	> $OFILE_COLORS
 fi
 
 # Bang!
-echo '#!/bin/bash' >> $OFILE
-echo -e "\n\necho -e \"" >> $OFILE
+echo -e '#!/bin/bash\n' | tee --append $OFILE $OFILE_COLORS &> /dev/null
+echo -e "\necho -e \"" | tee --append $OFILE &> /dev/null
 
 # \x1b is a control character changing behaviour of the shell.
 # It is also the <Ctrl+V><Esc> sequence.
 for i in {016..255}; do
 	echo -e "\x1b[38;5;${i}m $OTEXT $i \x1b[0m" >> $OFILE
+	echo -e "color${i}=\"\[\033[38;5;${i}m\]\"" >> $OFILE_COLORS
 done
 
 # End of echo.
-echo '"' >> $OFILE
+echo '"' | tee --append $OFILE &> /dev/null
 
 # The file should be executable.
 chmod +x $OFILE
